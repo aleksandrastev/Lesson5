84f3ba8781fbb1da3dacd76258e539f32f90aff2
aleksandrastev <leafsoul91@gmail.com>
2019-05-19 16:02:04 +0200

    Code added

--- src/optional/LandOfTurtles.java
+++ src/optional/LandOfTurtles.java
@@ -1,6 +1,10 @@
 package optional;
 
+import java.util.Random;
+
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 import org.teachingextensions.logo.MultiTurtlePanel;
 import org.teachingextensions.logo.Turtle;
 
@@ -10,18 +14,33 @@ public class LandOfTurtles {
 		String galapagosIslands = "https://farm2.staticflickr.com/1104/752631367_5c5d474ba5_o.jpg";
 
 		// 1. Instantiate a JFrame object & make it visible
+		JFrame frame = new JFrame();
+		frame.setVisible(true);
 
 		// 2. Add the panel to the frame
+		frame.getContentPane().add(panel);
 
 		// 3. Set the background image of the panel to the Galapagos Islands
+		panel.setBackgroundImage(galapagosIslands);
 
-		//4. Set the size image of the frame to 600 by 400 
+		// 4. Set the size image of the frame to 600 by 400
+		frame.setSize(600, 400);
 
 		// 5. Instantiate a Turtle
+		Turtle turtle = new Turtle();
 
-		// 6. Add the turtle to the panel 
+		// 6. Add the turtle to the panel
+		panel.addTurtle(turtle);
 
 		// 7. Put 50 Turtles on the beach
+		Random random = new Random();
+		Turtle tur[] = new Turtle[50];
+		for (int i = 0; i < tur.length; i++) {
+			tur[i] = new Turtle();
+			panel.addTurtle(tur[i]);
+			tur[i].setX(random.nextInt(300) + 300);
+			tur[i].setY(random.nextInt(250) + 150);
+		}
 
 	}
 }
